8e5c6c1bbbe45c46c1607a8cf4c95ea93f97b36f
fitriyani20 <147709585+fitriyani20@users.noreply.github.com>
2025-06-03 16:05:40 +0700

    Add files via upload

---
+++ Pertemuan_14/TestCounter.java
@@ -0,0 +1,11 @@
+package Pertemuan_14;
+//TUGAS 3
+public class TestCounter {
+
+	    public static void main(String[] args) {
+	        Counter counter1 = new Counter();
+	        Counter counter2 = new Counter();
+	        Counter counter3 = new Counter();
+	        System.out.println("Instance Count: " + Counter.getInstanceCount());
+	    }
+	}
---
+++ Pertemuan_14/TestDatabaseConnection.java
@@ -0,0 +1,9 @@
+package Pertemuan_14;
+
+//TUGAS 4
+	public class TestDatabaseConnection {
+	    public static void main(String[] args) {
+	        // Memanggil kelas DatabaseConnection untuk menginisialisasi koneksi
+	        System.out.println("Connection String: " + DatabaseConnection.connectionString);
+	    }
+	}
